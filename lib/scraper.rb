75fc90e509d0d0a1b731cb7da1c09eee90d0c53b
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-06 19:59:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,9 +27,12 @@ class Scraper
   end
   
   def make_courses
-    self.get_courses.each do |post|
+    doc.css(".post").each do |post|
       course = Course.new
-      Course.title = post.css("h2")
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
   
 end
